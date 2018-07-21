24cebfaef93f3a9a59d35fbdcd2f662ae00b9c3a
watiko <service@mail.watiko.net>
2018-07-22 00:44:15 +0900

    build: split a rewire function package

--- packages/app-a/config-overrides.js
+++ packages/app-a/config-overrides.js
@@ -1,23 +1,4 @@
-const { paths, getLoader } = require('react-app-rewired');
-const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const { compose } = require('react-app-rewired');
+const { rewireAllowDirectImportTs } = require('@app/rewire');
 
-const regexEquals = (x, y) => x.toString() === y.toString();
-
-module.exports = function override(config, env) {
-  const tsLoader = getLoader(
-    config.module.rules,
-    rule => rule.test && regexEquals(rule.test, /\.(ts|tsx)$/)
-  );
-
-  const plugins = [
-    // new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
-    new TsconfigPathsPlugin({ configFile: paths.appTsConfig }),
-  ];
-
-  // mutations
-  delete tsLoader.include;
-  config.resolve.plugins = plugins;
-
-  return config;
-}
+module.exports = compose(rewireAllowDirectImportTs);
---
+++ packages/lib-rewire/index.js
@@ -0,0 +1,27 @@
+const { paths, getLoader } = require('react-app-rewired');
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+const regexEquals = (x, y) => x.toString() === y.toString();
+
+function rewireAllowDirectImportTs(config, env) {
+  const tsLoader = getLoader(
+    config.module.rules,
+    rule => rule.test && regexEquals(rule.test, /\.(ts|tsx)$/)
+  );
+
+  const plugins = [
+    // new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
+    new TsconfigPathsPlugin({ configFile: paths.appTsConfig }),
+  ];
+
+  // mutations
+  delete tsLoader.include;
+  config.resolve.plugins = plugins;
+
+  return config;
+}
+
+module.exports = {
+  rewireAllowDirectImportTs,
+};
