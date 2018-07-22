b892e0091ba27cbe178ff3eb6e35214e6aff79cd
watiko <service@mail.watiko.net>
2018-07-23 01:27:46 +0900

    feat(rewire): add logging util

--- packages/lib-rewire/index.js
+++ packages/lib-rewire/index.js
@@ -3,6 +3,17 @@ const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const rewireCssModules = require('react-app-rewire-css-modules');
 
+/**
+ * USAGE: compose(createLogger('/tmp/webpack.config.json'), ...)
+ * @param {string} outputPath
+ */
+const createLogger = (outputPath) => (config, _env) => {
+    const fs = require('fs');
+    const replacer = (_, v) => v instanceof RegExp ? v.toString() : v;
+    fs.writeFileSync(outputPath, JSON.stringify(config, replacer, 2));
+    return config;
+  }
+
 const regexEquals = (x, y) => x.toString() === y.toString();
 
 function rewireAllowDirectImportTs(config, env) {
@@ -24,6 +35,7 @@ function rewireAllowDirectImportTs(config, env) {
 }
 
 module.exports = {
+  createLogger,
   rewireAllowDirectImportTs,
   rewireCssModules,
 };
